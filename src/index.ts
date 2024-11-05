7be3188c50a4eb33878513f8abed2c604372d5f0
carlos123asd <carlos-medin@hotmail.com>
2024-11-05 23:30:35 +0100

    ADD: workflows

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,65 @@
-import dotenv from 'dotenv'
-import {Application} from 'express'
-import express from 'express'
-import routerRoom from './controllers/Room'
-import routerBooking from './controllers/Booking'
-import routerMessage from './controllers/Message'
-import routerUser from './controllers/User'
-import routerAuth from './controllers/Auth'
-import cors from 'cors'
-import swaggerUi from 'swagger-ui-express'
-import swaggerDocument from '../swagger-output.json'
-import { connectDB } from './db/conectionDB'
+import dotenv from 'dotenv';
+import { Application } from 'express';
+import express from 'express';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger-output.json';
+import { connectDB } from './db/conectionDB';
+import routerRoom from './controllers/Room';
+import routerBooking from './controllers/Booking';
+import routerMessage from './controllers/Message';
+import routerUser from './controllers/User';
+import routerAuth from './controllers/Auth';
+import serverless from 'serverless-http';
 
-dotenv.config()
+dotenv.config();
 
-const app:Application = express()
-const port = process.env.PORT || "3000"
+const app: Application = express();
+const port = process.env.PORT || "3000";
 const apiPaths = {
     rooms: '/rooms',
     bookings: '/bookings',
     messages: '/messages',
     users: '/users',
-}
-/*función de seguridad que permite a los navegadores web realizar solicitudes
-a un dominio diferente al que sirve la página web . Sin CORS, los navegadores
-restringen dichas solicitudes debido a cuestiones de seguridad.*/
-app.use(cors())
-app.use(express.json())
+};
 
-app.get('/',(req,res) => {
+/* Configuración de CORS */
+app.use(cors());
+app.use(express.json());
+
+/* Ruta de inicio */
+app.get('/', (req, res) => {
     res.send({
         msg: "Login"
-    })
-})
-//Rutas Auth
-app.use('/auth',routerAuth)
+    });
+});
 
-//Rutas generales
-app.use(apiPaths.rooms,routerRoom)
-app.use(apiPaths.bookings,routerBooking)
-app.use(apiPaths.messages,routerMessage)
-app.use(apiPaths.users,routerUser)
+// Rutas de autenticación
+app.use('/auth', routerAuth);
 
-//Documentacion Rutas SwagGer
+// Rutas generales de la API
+app.use(apiPaths.rooms, routerRoom);
+app.use(apiPaths.bookings, routerBooking);
+app.use(apiPaths.messages, routerMessage);
+app.use(apiPaths.users, routerUser);
+
+// Documentación Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-      
 
+/* Función para iniciar la conexión a la base de datos y el servidor */
 const startServer = async () => {
     try {
-        await connectDB()
-        app.listen(port, () => console.log('Server listen on port 3000'))
+        await connectDB();
+        app.listen(port, () => console.log(`Server listening on port ${port}`));
     } catch (error) {
-        console.error(error)
-        process.exit(1)
+        console.error(error);
+        process.exit(1);
     }
+};
+
+// Verifica si está en un entorno de Lambda o en local
+if (process.env.IS_OFFLINE) {
+    startServer();
 }
 
-startServer()
+export const handler = serverless(app);  //adaptador de serverless para Lambda
