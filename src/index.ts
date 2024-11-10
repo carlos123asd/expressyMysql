9f34ba7c853b0f73c3c8f042571b8711d84f2054
carlos123asd <carlos-medin@hotmail.com>
2024-11-10 21:51:52 +0100

    SOL2: CORS

--- src/index.ts
+++ src/index.ts
@@ -27,17 +27,20 @@ const apiPaths = {
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://example.com');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+})
+
 /* Ruta de inicio */
 app.get('/', (req, res) => {
     res.send({
         msg: "Wellcome API Miranda"
     });
 });
-
-app.use((req, res, next) => {
-    res.header({"Access-Control-Allow-Origin": "*"});
-    next();
-})
 
 // Rutas de autenticación
 app.use('/auth', routerAuth);
