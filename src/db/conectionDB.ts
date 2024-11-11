670879c3e7820877513092814232e5cb55f806dc
carlos123asd <carlos-medin@hotmail.com>
2024-11-11 16:16:05 +0100

    Timeout

--- src/db/conectionDB.ts
+++ src/db/conectionDB.ts
@@ -10,6 +10,7 @@ export const connectDB = async (): Promise<Connection> => {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       port: Number(process.env.DB_PORT) || 3306,
+      connectTimeout: 10000,
     });
     console.log('Conectado a la base de datos MYSQL');
   }
