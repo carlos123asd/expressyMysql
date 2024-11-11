cbe57b378117aa6d62ecd949221b3dfe35d26049
carlos123asd <carlos-medin@hotmail.com>
2024-11-11 19:00:43 +0100

    test2

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,18 @@ app.use((req, res, next) => {
     next();
 })
 
+const bufferToJSONMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+	if (req.body.length > 0 && req.body instanceof Buffer) {
+	try {
+		req.body = JSON.parse(req.body.toString());
+	} catch (err) {
+		return res.status(400).json({ body: req.body, length: req.body.length, error: 'Invalid JSON data' });
+	}
+	}
+
+	next();
+};
+app.use(bufferToJSONMiddleware);
 /* Ruta de inicio */
 app.get('/', (req, res) => {
     res.send({
