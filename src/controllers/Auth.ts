806c875365aa27689983a7e77e7ff249db5317f8
carlos123asd <carlos-medin@hotmail.com>
2024-11-11 19:09:17 +0100

    test3

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -21,6 +21,8 @@ async function authenticate (req:Request, res:Response) {
       return res.status(400).json({message: 'User not found'})
     }else{
       const user = userFound as Employee[];
+      console.log('ini',password);
+      console.log('comp',user[0].password);
       const isAuth = await bcrypt.compare(password,user[0].password)
       if(!isAuth){
         return res.status(400).json({message: 'Password Incorrect'})
